dce93359a5bb8399368f752ed66b68e85c9b2fff
witekdev <witekdev@gmail.com>
2018-05-08 23:41:36 -0400

    update version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.siteimprove',
-    version='1.0a1',
+    version='0.1alpha',
     description="Siteimprove integration for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
