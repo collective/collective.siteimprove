c9d7278a45f8bc217a2b5c298a2811b835050b66
Alec Mitchell <alecpm@gmail.com>
2018-05-22 15:35:28 -0700

    Call input method on visible page loads

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.siteimprove',
-    version='0.1alpha',
+    version='0.1a2',
     description="Siteimprove integration for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
