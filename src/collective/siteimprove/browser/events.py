79e8cca86bda0406d4a574d9d5a440f144294019
witekdev <witekdev@gmail.com>
2018-06-17 09:34:26 -0600

    Publishing a document will trigger an automatic site improve recheck

---
+++ src/collective/siteimprove/browser/events.py
@@ -0,0 +1,14 @@
+from collective.siteimprove.interfaces import ICollectiveSiteimproveLayer
+from zope.globalrequest import getRequest
+from zope.component.hooks import getSite
+
+def triggerSiteimproveRecheck(obj, event):
+    """ Set flag on request for viewlet to trigger siteimprove 
+        recheck
+    """
+    if ICollectiveSiteimproveLayer.providedBy(getRequest()):
+        session_manager = getSite().session_data_manager
+        session = session_manager.getSessionData()
+        if event.action == 'publish':
+            # set flag in the session
+            session['SI-Published'] = True
--- src/collective/siteimprove/browser/viewlets.py
+++ src/collective/siteimprove/browser/viewlets.py
@@ -3,7 +3,6 @@ from plone.app.layout.viewlets import common as base
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
 
-
 class SiteimproveJavascriptViewlet(base.ViewletBase):
     """ This will render Siteimprove JavaScript load in <head>.
         <head> section is retrofitted only if the viewlet is enabled.
@@ -13,9 +12,17 @@ class SiteimproveJavascriptViewlet(base.ViewletBase):
         super(SiteimproveJavascriptViewlet, self).update()
 
         self.token = None
+        self.recheck = False
 
         # lookup token
         registry = getUtility(IRegistry)
         siteimprove_registry = registry.forInterface(ISiteimproveSchema, False)
         if siteimprove_registry and siteimprove_registry.token:
             self.token = siteimprove_registry.token
+
+        session_manager = self.context.session_data_manager
+        session = session_manager.getSessionData()
+        if session.has_key('SI-Published'):
+            self.recheck = True
+            # clear flag in the session
+            del session['SI-Published']
