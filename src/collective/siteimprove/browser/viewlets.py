19b8b508f0583aff2df0eccb62c7fba36052bfaa
witekdev <witekdev@gmail.com>
2018-06-17 09:42:29 -0600

    fix whitespace

--- src/collective/siteimprove/browser/viewlets.py
+++ src/collective/siteimprove/browser/viewlets.py
@@ -3,6 +3,7 @@ from plone.app.layout.viewlets import common as base
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
 
+
 class SiteimproveJavascriptViewlet(base.ViewletBase):
     """ This will render Siteimprove JavaScript load in <head>.
         <head> section is retrofitted only if the viewlet is enabled.
