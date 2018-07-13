dca5e8a198e54273e48a7f771c62a362c482ff46
Alec Mitchell <alecpm@gmail.com>
2018-07-13 16:34:15 -0700

    New beta relase.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.siteimprove',
-    version='0.1b1',
+    version='0.1b2',
     description="Siteimprove integration for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
