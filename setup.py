4b316bc68224b40e99dd7612903008fc6f9c19fd
Alec Mitchell <alecpm@gmail.com>
2018-09-19 15:28:53 -0700

    Prepare 1.0.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.siteimprove',
-    version='1.0.1',
+    version='1.0.2',
     description="Siteimprove integration for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
